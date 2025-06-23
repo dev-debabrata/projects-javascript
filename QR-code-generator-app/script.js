8e5e3a8d8a2569b63c946f673404808df1088d4e
dev-debabrata <debabratadas711@gmail.com>
2025-06-23 12:57:28 +0530

    modified code

--- QR-code-generator-app/script.js
+++ QR-code-generator-app/script.js
@@ -1,1 +1,50 @@
 const qrText = document.getElementById('qr-text');
+const sizes = document.getElementById('sizes');
+const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+const qrContainer = document.querySelector('.qr-body');
+
+let size = sizes.value;
+generateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    isEmptyInput();
+});
+
+sizes.addEventListener('change', (e) => {
+    size = e.target.value;
+    isEmptyInput();
+});
+
+
+// Download image
+downloadBtn.addEventListener('click', () => {
+    let img = document.querySelector('.qr-body img');
+
+    if (img !== null) {
+        let imgAtrr = img.getAttribute('src');
+        downloadBtn.setAttribute("href", imgAtrr);
+    }
+    else {
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`);
+    }
+});
+
+
+
+// Alert popup
+function isEmptyInput() {
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");;
+}
+
+
+// QR generator
+function generateQRCode() {
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer, {
+        text: qrText.value,
+        height: size,
+        width: size,
+        colorLight: "#fff",
+        colorDark: "#000",
+    });
+}
